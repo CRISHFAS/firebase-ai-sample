fe77a5f2179908f4a789173baede5670deb88888
jerybbkuuukkrjhwioio <jerybbkuuukkrjhwioio@gmail.com>
2024-12-21 04:11:35 +0000

    Initial commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyCVj3JmeIYocwaBptNqlCfXseJ8cpPuAvI',
+      authDomain: 'sample-firebase-ai-app-66069.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'sample-firebase-ai-app-66069',
+      storageBucket: 'sample-firebase-ai-app-66069.firebasestorage.app',
+      messagingSenderId: '160807342161',
+      appId: '1:160807342161:web:d3fe8615083c8cb8fba870',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyAA4cymYF1Hws2XO8GMj9TtX23R1sUg-lY',
+  debug_token: ''
+};
